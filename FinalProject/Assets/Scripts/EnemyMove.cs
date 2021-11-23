ec8b50d817e32a052c261976a0627c29363b6ee4
Miles <mae9775@nyu.edu>
2021-11-23 09:14:26 -0500

    added enemy HP and response to slash tag

--- FinalProject/Assets/Scripts/EnemyMove.cs
+++ FinalProject/Assets/Scripts/EnemyMove.cs
@@ -8,6 +8,8 @@ public class EnemyMove : MonoBehaviour
 
     public GameObject platform;
 
+    int enemyHP = 3;
+
     float platformPosRight;
     float platformWidth;
     float initObjX;
@@ -17,25 +19,39 @@ public class EnemyMove : MonoBehaviour
     void Start()
     {
 
-        initObjX = gameObject.transform.position.x;
+        initObjX = gameObject.transform.position.x; //get the starting pos
 
-        platformWidth = platform.GetComponent<SpriteRenderer>().bounds.size.x;
-        platformPosRight = initObjX + (platformWidth/platformAdjust);
+        platformWidth = platform.GetComponent<SpriteRenderer>().bounds.size.x; //get width of platform its on
+        platformPosRight = initObjX + (platformWidth/platformAdjust); //add width of platform to start pos
     }
 
     void Update()
     {
         //PingPong between 0 and 1
-        float time = Mathf.PingPong(Time.time * speed, 1);
+        float time = Mathf.PingPong(Time.time * speed, 1); 
         transform.position = Vector3.Lerp( new Vector3(initObjX, gameObject.transform.position.y , gameObject.transform.position.z) , new Vector3 (platformPosRight, gameObject.transform.position.y , gameObject.transform.position.z), time);
     }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player") // if collide with player subtract its hp
         {
             collision.gameObject.GetComponent<PlayerHP>().health -= 1;
+        }
+
+        if(collision.gameObject.tag == "Slash")
+        {
+            enemyHP -= 1;
+        }
+    }
+
+
+    void enemyDie()
+    {
+        if(enemyHP == 0)
+        {
+            Destroy(gameObject);
         }
     }
 
