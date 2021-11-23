af019538a86acfd5ca7d839b6b52cc9201cdfa09
Miles <mae9775@nyu.edu>
2021-11-23 15:21:49 -0500

    Added HP UI

--- FinalProject/Assets/Scripts/PlayerHP.cs
+++ FinalProject/Assets/Scripts/PlayerHP.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
 
     public int health;
+    public int healthMax;
+    public int hpDisplayed;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
 
     private void Start()
     {
@@ -16,6 +24,31 @@ public class PlayerHP : MonoBehaviour
     void Update()
     {
         Debug.Log(health);
+
+        if(health > healthMax)
+        {
+            health = healthMax;
+        }
+
+        for(int i = 0; i < hearts.Length; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            } else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+
+            if(i < hpDisplayed)
+            {
+                hearts[i].enabled = true;
+            } else
+            {
+                hearts[i].enabled = false;
+            }
+        }
 
     }
 
