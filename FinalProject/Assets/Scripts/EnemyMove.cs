bfc72f1269e632511b0360b558d3f177a4b86f24
Miles <mae9775@nyu.edu>
2021-11-22 19:58:28 -0500

    Enemy Auto Move

---
+++ FinalProject/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float speed = 1.19f;
+
+    public GameObject platform;
+
+    float platformPosRight;
+    float platformWidth;
+    float initObjX;
+
+    public float platformAdjust;
+
+    void Start()
+    {
+
+        initObjX = gameObject.transform.position.x;
+
+        platformWidth = platform.GetComponent<SpriteRenderer>().bounds.size.x;
+        platformPosRight = initObjX + (platformWidth/platformAdjust);
+    }
+
+    void Update()
+    {
+        //PingPong between 0 and 1
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp( new Vector3(initObjX, gameObject.transform.position.y , gameObject.transform.position.z) , new Vector3 (platformPosRight, gameObject.transform.position.y , gameObject.transform.position.z), time);
+    }
+}
