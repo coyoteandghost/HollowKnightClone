7ac683711bcc584ac13c95d9dea83f31b85b2061
Miles <mae9775@nyu.edu>
2021-11-30 10:59:14 -0500

    made spikes less dumb

--- FinalProject/Assets/Scripts/SpikeBehavior.cs
+++ FinalProject/Assets/Scripts/SpikeBehavior.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class SpikeBehavior : MonoBehaviour
 {
 
+ 
+
+    public GameObject teleportSpot;
     public Vector3 safePos;
+
    // public Vector3 checkpointSafePos;
    // public GameObject player;
 
@@ -14,6 +18,8 @@ public class SpikeBehavior : MonoBehaviour
         if(collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<PlayerHP>().health -= 1; //whenplayer gets hit, minus health
+
+            safePos = new Vector3(teleportSpot.transform.position.x, teleportSpot.transform.position.y, 0);
 
             collision.gameObject.GetComponent<Transform>().position = safePos; //sends player to safePos
         }
